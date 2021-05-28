39ab39faaa0645c2c691ac7d264998bb73f24e90
Zahra1205 <78679374+Zahra1205@users.noreply.github.com>
2021-05-28 14:03:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+menu_list_array = ["Veg Margherita Pizza",
+"Chicken Tandoori Pizza",
+"Veg Suprema Pizza",
+"Panner Tikka Pizza",
+"Deluxe Veggie Pizza",
+"Spicy Triple Tango",
+"Golden Corn Pizza",
+"Margherita Pizza",
+"Pepperoni Pizza"
+];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort()''
+for(var i=0;i<menu_list_array.lenght;i++){
+    htmldata=htmldata+'<li>'+menu_list_array[i]+'</li>'
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML=htmldata;
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+menu_list_array.push(item);
+htmldata="section class='cards'>"
+for (var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="card>'
+    +'<img src="images/pizzaImg.png"/>'
+    +menu_list_array[i]+'</div>'
+}
+
+
+function add_top(){
+var item=document.getElementById("add_item").value;
+menu_list_array.push(item);
+add_item();
+}
